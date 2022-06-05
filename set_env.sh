0896f0d7e15f2f870bae999f880288b6c3292933
Victor Tejada Yau <victortyau@gmail.com>
2022-06-05 18:56:40 -0500

    working on microservices

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+ # DO NOT STORE YOUR CREDENTIALS INTO GIT
+ export POSTGRES_USERNAME=udagram_user
+ export POSTGRES_PASSWORD=udagram-password
+ export POSTGRES_HOST=udagramdev.csxehfv0oft6.us-east-2.rds.amazonaws.com
+ export POSTGRES_DB=udagramdev
+ export AWS_BUCKET=arn:aws:s3:::microservice-bucket-dk
+ export AWS_REGION=us-east-2
+ export AWS_PROFILE=udagramdev
+ export JWT_SECRET=testing
+ export URL=http://localhost:8100
